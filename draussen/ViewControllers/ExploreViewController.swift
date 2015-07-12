472af56c4f8a28199d2f670d0fbf75d635dfd44d
Timothy Horng <thorng@asu.edu>
2015-07-11 23:04:21 -0700

    started "explore" section
    Laid out basic storyboard

---
+++ draussen/ViewControllers/ExploreViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreViewController.swift
+//  draussen
+//
+//  Created by Timothy Horng on 7/11/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
