3c725c76b250299f47c4cab576ba217b8ec7631d
indigothm <rbkshadow@gmail.com>
2015-07-12 00:17:55 -0700

    Error fixed

--- draussen/ViewControllers/ExploreViewController.swift
+++ draussen/ViewControllers/ExploreViewController.swift
@@ -15,10 +15,4 @@ class ExploreViewController: UIViewController {
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
